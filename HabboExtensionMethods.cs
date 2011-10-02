de351e6846145bc3e345cde6f9da29eeaa2c8d74
Chris Chenery <Cecer1@cecer1.com>
2011-10-02 21:57:27 +0100

    Restyled

---
+++ HabboExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using IHI.Server.Habbos;
+using IHI.Server.Networking.Messages;
+
+namespace IHI.Server.Plugins.Cecer1.UserHandlers
+{
+    public static class HabboExtensionMethods
+    {
+        public static int GetVolume(this Habbo habbo)
+        {
+            int volume;
+
+            if (!int.TryParse(habbo.GetPersistantVariable("Client.Volume"), out volume))
+            {
+                volume = 5; // TODO: Default value in config?
+                habbo.SetPersistantVariable("Client.Volume", volume.ToString());
+            }
+            return volume;
+        }
+
+        public static Habbo SetVolume(this Habbo habbo, int volume)
+        {
+            habbo.SetPersistantVariable("Client.Volume", volume.ToString());
+            new MVolumeLevel(volume).Send(habbo); // Update the client.
+            return habbo;
+        }
+    }
+}
---
+++ UserHandlers.cs
@@ -0,0 +1,56 @@
+﻿using IHI.Server.Habbos;
+using IHI.Server.Networking.Messages;
+
+namespace IHI.Server.Plugins.Cecer1.UserHandlers
+{
+    public class UserHandlers : Plugin
+    {
+        public override void Start()
+        {
+            Habbo.OnHabboLogin += RegisterHandlers;
+        }
+
+        private static void RegisterHandlers(object source, HabboEventArgs e)
+        {
+            var target = (source as Habbo);
+
+            // Is source of type Habbo?
+            if (target == null)
+                return;
+
+            target.
+                GetConnection().
+                AddHandler(6, PacketHandlerPriority.DefaultAction, ProcessBalanceRequest).
+                AddHandler(7, PacketHandlerPriority.DefaultAction, ProcessUserInfoRequest).
+                AddHandler(8, PacketHandlerPriority.DefaultAction, ProcessGetVolumeLevel).
+                AddHandler(228, PacketHandlerPriority.DefaultAction, ProcessGetVolumeLevel).
+                AddHandler(482, PacketHandlerPriority.DefaultAction, ProcessUnknown);
+        }
+
+        private static void ProcessUserInfoRequest(Habbo sender, IncomingMessage message)
+        {
+            new MUserData(sender).Send(sender);
+        }
+
+        private static void ProcessBalanceRequest(Habbo sender, IncomingMessage message)
+        {
+            new MCreditBalance(sender).Send(sender);
+        }
+
+        private static void ProcessGetVolumeLevel(Habbo sender, IncomingMessage message)
+        {
+            new MVolumeLevel(sender.GetVolume()).Send(sender);
+        }
+
+
+        //TODO: Figure this out.
+        private static void ProcessUnknown(Habbo sender, IncomingMessage message)
+        {
+            CoreManager.GetServerCore().GetStandardOut().
+                PrintDebug("Part 1: " + message.PopPrefixedString()).
+                PrintDebug("Part 2: " + message.PopPrefixedString()).
+                PrintDebug("Part 3: " + message.PopPrefixedString()).
+                PrintDebug("Part 4: " + message.PopPrefixedString());
+        }
+    }
+}
